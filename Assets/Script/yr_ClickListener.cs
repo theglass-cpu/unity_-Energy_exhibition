53b87cf7e253831fef4ada0814881e5d32a4029e
Yuri Jung <dfdbfg@naver.com>
2021-12-28 16:52:23 +0900

    저유리정유리

--- Assets/Script/yr_ClickListener.cs
+++ Assets/Script/yr_ClickListener.cs
@@ -9,14 +9,18 @@ public class yr_ClickListener : MonoBehaviour
     // 클릭하고 싶은 오브젝트
     public GameObject clickObject;
 
-    public GameObject yr_water_point, yr_gas_point;
+    public GameObject yr_water_point, yr_gas_point, yr_bituminous_coal_point, yr_new_renewable_energy_point, yr_nuclear_power_point;
 
 
     private void Start()
     {
         yr_water_point.SetActive(false);
         yr_gas_point.SetActive(false);
-       
+        yr_bituminous_coal_point.SetActive(false);
+        yr_new_renewable_energy_point.SetActive(false);
+        yr_nuclear_power_point.SetActive(false);
+
+
     }
 
     public void btn_Click()
@@ -31,12 +35,52 @@ public class yr_ClickListener : MonoBehaviour
             case "yr_water_power_bt":
                 Debug.Log("수력버튼클릭함");
                 yr_water_point.SetActive(true);
+
                 yr_gas_point.SetActive(false);
+                yr_bituminous_coal_point.SetActive(false);
+                yr_new_renewable_energy_point.SetActive(false);
+                yr_nuclear_power_point.SetActive(false);
+
+
                 break;
             case "yr_gas_bt":
                 Debug.Log("가스버튼클릭함");
-                yr_water_point.SetActive(false);
                 yr_gas_point.SetActive(true);
+
+                yr_water_point.SetActive(false);
+                yr_bituminous_coal_point.SetActive(false);
+                yr_new_renewable_energy_point.SetActive(false);
+                yr_nuclear_power_point.SetActive(false);
+                break;
+
+            case "yr_nuclear_power_bt":
+                Debug.Log("원자력버튼클릭함");
+
+                yr_nuclear_power_point.SetActive(true);
+
+                yr_water_point.SetActive(false);
+                yr_gas_point.SetActive(false);
+                yr_bituminous_coal_point.SetActive(false);
+                yr_new_renewable_energy_point.SetActive(false);
+                break;
+            case "yr_new_renewable_energy_bt":
+                Debug.Log("신재생버튼클릭함");
+
+                yr_new_renewable_energy_point.SetActive(true);
+
+                yr_nuclear_power_point.SetActive(false);
+                yr_water_point.SetActive(false);
+                yr_gas_point.SetActive(false);
+                yr_bituminous_coal_point.SetActive(false);
+
+                break;
+            case "yr_bituminous_coal_bt":
+                Debug.Log("유연탄버튼클릭함");
+                yr_water_point.SetActive(false);
+                yr_gas_point.SetActive(false);
+                yr_bituminous_coal_point.SetActive(true);
+                yr_new_renewable_energy_point.SetActive(false);
+                yr_nuclear_power_point.SetActive(false);
                 break;
 
         }
