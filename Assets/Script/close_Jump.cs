5fa21c753eb46d2274dda83ea0cd3679aead09db
yiroze <ghwo2032@naver.com>
2022-01-01 15:24:07 +0900

    배치 완료

--- Assets/Script/close_Jump.cs
+++ Assets/Script/close_Jump.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class close_Jump : MonoBehaviour
 {
     public Transform other;
+    public string name;
 
     // Start is called before the first frame update
     void Start()
@@ -23,12 +25,14 @@ public class close_Jump : MonoBehaviour
        
         float distance = Vector3.Distance(other.position, transform.position);
 
+        if (name == "hj")
+        {
         if (distance < 50f)
         {
-            GameObject.Find("Main Camera").GetComponent<Scene_Jumping>().Jump();
+                SceneManager.LoadScene("hy_scene2");
 
+            }
         }
-
 
         print("다른 사람과의 거리: " + distance);
 
