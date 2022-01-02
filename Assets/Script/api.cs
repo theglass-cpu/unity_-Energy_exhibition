7631838c925f8b1aa843acf34bedee73d7d5759b
yiroze <ghwo2032@naver.com>
2022-01-02 11:20:36 +0900

    미니어쳐 이름바꾸는 중

--- Assets/Script/api.cs
+++ Assets/Script/api.cs
@@ -18,16 +18,19 @@ public class api : MonoBehaviour
 
     void LoadData() //json 문자열 받아오기
     {
+
         string GetDataUrl = "http://apis.data.go.kr/1611000/BldEngyService/getBeElctyUsgInfo?serviceKey=DWbrLR2ujfse1XuO8PAws4RslOtGNQJm4GfVplJ07iblYuUmK%2BnIy9SneMX5acXUUZrzsNkfI1dOX1daJNyBbQ%3D%3D&numOfRows=1&pageNo=1&sigunguCd=11680&bjdongCd=10300&bun=0012&ji=0000&useYm=202109";
         XmlDocument xml = new XmlDocument();
         xml.Load(GetDataUrl);
         XmlNodeList xnList = xml.GetElementsByTagName("item"); //접근할 노드
+
 
         foreach (XmlNode xn in xnList)
         {
             string useQty = xn["useQty"].InnerText;
             Debug.Log(useQty);
         }
+
 
     }
 
